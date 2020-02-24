75016e9176c310d71600c2a285354cbac58a8b3b
Deepan Raj Kumar <princedeepan@gmail.com>
2020-02-24 17:37:48 +0530

    upi filter

---
+++ android/app/src/main/java/com/nativetest/UpiFilter.java
@@ -0,0 +1,14 @@
+package com.nativetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpiFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upi_filter);
+    }
+}
